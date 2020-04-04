694b4dd7777f6e8f13ce20b83092f0a129ec8adb
Shaoor Munir <shaoormunir@outlook.com>
2020-04-04 19:19:41 +0500

    changed parameters

--- train_discriminator.sh
+++ train_discriminator.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 export GOOGLE_APPLICATION_CREDENTIALS=/content/iowa-project-2.json
 
-input_file="/content/input-data/complete_dataset.json"
-output_dir="gs://model_checkpoint_bucket/grover-discrimination-pretrained-large-run-12"
-init_checkpoint="gs://model_checkpoint_bucket/grover-discrimination-pretrained-large-run-11"
-model_type="large"
+input_file="/content/input-data/dataset_train.json"
+output_dir="gs://model_checkpoint_bucket/grover-discriminator-run-1"
+init_checkpoint="gs://model_checkpoint_bucket/grover-discriminator-pretrained-base"
+model_type="base"
 
 python run_discrimination.py \
     --config_file=/content/grover/lm/configs/${model_type}.json \
